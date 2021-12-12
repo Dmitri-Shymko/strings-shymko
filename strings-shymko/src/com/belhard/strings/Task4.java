96c1e32b7241809457f42efdc26b5dd463633dbc
Dmitri Shymko <shim-2006@tut.by>
2021-12-12 15:06:32 +0300

    Task4 - edited

--- strings-shymko/src/com/belhard/strings/Task4.java
+++ strings-shymko/src/com/belhard/strings/Task4.java
@@ -1,14 +1,21 @@
 package com.belhard.strings;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task4 {
 
 	public static void main(String[] args) {
-		String str = DemoStrings.NUMBERS.replaceAll("[^0-9]+", " ").trim()
-				.replaceAll("[0-9]+", "1")
-				.replaceAll("\\s+","");
-		
-		System.out.println("Number of numbers is: " + str.length());
-		
+
+		Pattern pattern = Pattern.compile("[+-]?([0-9]*[.])?[0-9]+");
+		Matcher matcher = pattern.matcher(DemoStrings.NUMBERS);
+
+		int numberOfNumbers = 0;
+		while (matcher.find()) {
+			numberOfNumbers++;
+		}
+		System.out.println("Number of numbers is: " + numberOfNumbers);
+
 	}
 
 }
